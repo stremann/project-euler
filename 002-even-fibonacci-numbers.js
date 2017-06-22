034cf0a90f444909c5a3620840feccdef1dda200
Roman Stremedlovskyi <Roman_Stremedlovskyi@epam.com>
2017-06-22 15:56:15 +0300

    feat(002) add even fibonacci numbers

---
+++ 002-even-fibonacci-numbers.js
@@ -0,0 +1,20 @@
+function sum(n) {
+    let sum = 0;
+    let x = 1;  // Represents the current Fibonacci number being processed
+    let y = 2;  // Represents the next Fibonacci number in the sequence
+
+    while (x <= n) {
+        if (x % 2 === 0) {
+            sum += x;
+        }
+
+        let z = x + y;
+        x = y;
+        y = z;
+    }
+    return sum;
+}
+
+sum(4); // 2
+sum(400); // 188
+sum(4000000); // 4613732
