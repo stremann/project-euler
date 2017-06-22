161ee44f006ed0751aa738c03c4b5b41d41fcabd
Roman Stremedlovskyi <Roman_Stremedlovskyi@epam.com>
2017-06-22 15:57:25 +0300

    feat(001) add multiples of 3 and 5 solution

--- 001-multiples-of-3-and-5.js
+++ 001-multiples-of-3-and-5.js
@@ -2,11 +2,9 @@ function sum(n) {
     let sum = 0;
 
     for (let i = 0; i < n; i++) {
-
         if ((i % 3) === 0 || (i % 5) === 0) {
             sum += i;
         }
-
     }
 
     return sum;
